fd0d3c0ef221da2e9defa55acf85c5cf9c50ae34
thegreathir <aaph1377@gmail.com>
2023-01-23 18:54:01 +0330

    Add faster conflict counter

--- count_conflicts.cpp
+++ count_conflicts.cpp
@@ -7,26 +7,17 @@ inline unsigned int count_conflicts(const std::vector<std::vector<int>> &sudoku)
 {
     unsigned short counter[3][9][9] = {};
 
+    unsigned int result = 0;
+
     for (unsigned short i = 0; i < 9; ++i)
         for (unsigned short j = 0; j < 9; ++j)
         {
             unsigned int cell = sudoku[i][j] - 1;
-            counter[0][i][cell]++;
-            counter[1][j][cell]++;
-            counter[2][((i / 3) * 3) + (j / 3)][cell]++;
+            result += counter[0][i][cell]++;
+            result += counter[1][j][cell]++;
+            result += counter[2][((i / 3) * 3) + (j / 3)][cell]++;
         }
 
-    unsigned int result = 0;
-
-    for (unsigned short k = 0; k < 3; ++k)
-        for (unsigned short i = 0; i < 9; ++i)
-            for (unsigned short j = 0; j < 9; ++j)
-            {
-                unsigned short c = counter[k][i][j];
-                if (c > 1)
-                    result += (c * (c - 1));
-            }
-    
     return result;
 }
 
